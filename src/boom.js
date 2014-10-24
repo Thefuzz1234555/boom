aefac7a89e3c63fb3301901f56f5eba9184eb5ee
audibleblink <virtualsnyper@gmail.com>
2014-10-24 11:01:10 -0400

    refactor out a config object

--- src/boom.js
+++ src/boom.js
@@ -1,3 +1,14 @@
+CONFIG = {
+    bookmarklets: {
+        plex: "javascript:%20var%20s=document.createElement(%22script%22);s.type=%22text/javascript%22;s.src=%22https://my.plexapp.com/queue/bookmarklet_payload?uid=819f10b976818604%22;var%20h=document.getElementsByTagName(%22head%22)%5B0%5D;h.appendChild(s);void(0);",
+        pocket: "javascript:(function()%7BISRIL_H='edaa';PKT_D='getpocket.com';ISRIL_SCRIPT=document.createElement('SCRIPT');ISRIL_SCRIPT.type='text/javascript';ISRIL_SCRIPT.src='http://'+PKT_D+'/b/r.js';document.getElementsByTagName('head')%5B0%5D.appendChild(ISRIL_SCRIPT)%7D)();",
+        builtwith: "javascript:void(location.href='http://builtwith.com?'+location.href)",
+        whois: "javascript:void(location.href='https://who.is/whois/'+location.host)",
+        tdfw: "javascript:(function()%7Bjavascript:var%20s%3Ddocument.createElement(%27script%27)%3Bs.setAttribute(%27src%27,%27https://nthitz.github.io/turndownforwhatjs/tdfw.js%27)%3Bdocument.body.appendChild(s)%3B%7D)()%3B"
+    },
+    loadMoreUrl: "https://raw.githubusercontent.com/audibleblink/boom/ajax-configs/lib/bookmarkletsConfig.js"
+};
+
 Array.prototype.contains = function(obj) {
     var i = this.length;
     while (i--) {
@@ -12,13 +23,7 @@ Boom();
 
 function Boom() {
     var requestedBookmarklet = window.prompt('Boom: Which One');
-    var boomMarklets = {
-        plex: 'javascript:%20var%20s=document.createElement(%22script%22);s.type=%22text/javascript%22;s.src=%22https://my.plexapp.com/queue/bookmarklet_payload?uid=819f10b976818604%22;var%20h=document.getElementsByTagName(%22head%22)%5B0%5D;h.appendChild(s);void(0);',
-        pocket: "javascript:(function()%7BISRIL_H='edaa';PKT_D='getpocket.com';ISRIL_SCRIPT=document.createElement('SCRIPT');ISRIL_SCRIPT.type='text/javascript';ISRIL_SCRIPT.src='http://'+PKT_D+'/b/r.js';document.getElementsByTagName('head')%5B0%5D.appendChild(ISRIL_SCRIPT)%7D)();",
-        builtwith: "javascript:void(location.href='http://builtwith.com?'+location.href)",
-        whois: "javascript:void(location.href='https://who.is/whois/'+location.host)",
-        tdfw: "javascript:(function()%7Bjavascript:var%20s%3Ddocument.createElement(%27script%27)%3Bs.setAttribute(%27src%27,%27https://nthitz.github.io/turndownforwhatjs/tdfw.js%27)%3Bdocument.body.appendChild(s)%3B%7D)()%3B"
-    };
+    var boomMarklets = CONFIG['bookmarklets']
     var availableBookmarklets = Object.keys(boomMarklets);
 
     if (requestedBookmarklet === "help") {
