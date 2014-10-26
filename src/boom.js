80ba7a8c1a8e43250b8e727e922d2230aa6fce33
audibleblink <virtualsnyper@gmail.com>
2014-10-26 16:43:05 -0400

    appends necessary script to run
    - checks if script exists; skips append if it does
    - return Boom in a setTimeout to ensure script
      addition an Boom invocation occur on different
    event loops

--- src/boom.js
+++ src/boom.js
@@ -1,32 +1,51 @@
-sources = [
-    "//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js",
-    "//raw.githubusercontent.com/audibleblink/boom/ajax-configs/lib/bookmarkletsConfig.js"
-];
+var requestedBookmarklet = window.prompt('Boom: Enter your Bookmarklet\nType help to see all available options'),
+    sources = {
+        bookmarkletsConfig: "//rawgit.com/audibleblink/boom/ajax-configs/lib/bookmarkletsConfig.js",
+        jquery: "//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"
+    },
+    sourceKeys = Object.keys(sources);
 
-Boom();
+function insertMissingScripts(word){
+    var scriptList = document.getElementsByTagName('script'),
+        results = [];
 
-function insertJs(url){
+    for(var i = 0; i < scriptList.length; i++){
+        if (scriptList[i].src.match(new RegExp(word))){
+            results.push(true);
+        } else {
+            results.push(false);
+        }
+    }
+
+    if (results.indexOf(true) === -1){
+        appendScript(sources[word]);
+    }
+}
+
+function appendScript(url){
     var js = document.createElement('script');
     js.src = url;
     document.head.appendChild(js);
 }
 
-for(var i = 0; i < sources.length; i++){
-    insertJs(sources[i])
+
+for (var x = 0; x < sourceKeys.length; x++){
+    insertMissingScripts(sourceKeys[x]);
 }
 
-function Boom() {
-    var requestedBookmarklet = window.prompt('Boom: Enter your Bookmarklet\nType help to see all available options');
-    var boomMarklets = CONFIG['bookmarklets'];
-    var availableBookmarklets = Object.keys(boomMarklets);
-    console.log($.inArray(requestedBookmarklet, availableBookmarklets))
 
+setTimeout(function Boom() {
+    var boomMarklets = CONFIG,
+        availableBookmarklets = Object.keys(boomMarklets);
 
     if (requestedBookmarklet === "help") {
-        alert("The available bookmarklets for this version are:\n" + "\n" + availableBookmarklets.join("\n"))
+        alert("The available bookmarklets for this version are:\n" + "\n" + availableBookmarklets.join("\n"));
     } else if ($.inArray(requestedBookmarklet, availableBookmarklets) !== -1) {
         window.location = boomMarklets[requestedBookmarklet];
     } else {
-        alert("Bookmarklet not found")
+        alert("Bookmarklet not found");
     }
-};
+
+    return Boom;
+
+}, 200);
