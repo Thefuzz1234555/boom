0e8e09939ae922ef920385687c501bb01cca63d6
audibleblink <virtualsnyper@gmail.com>
2014-10-26 17:16:50 -0400

    add instruction for customization

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
+// replace with your username and branch to customize after forking
+var URL = rawGitUrl("audibleblink", "simplify");
+
+
 function rawGitUrl(ghHandle, branchName){
     return "//rawgit.com/" + ghHandle + "/boom/" + branchName + "/lib/boom.js";
 }
-
-var URL = rawGitUrl("audibleblink", "simplify");
 
 var js = document.createElement('script');
 js.src = URL;
